3885c616da272e00a583e72692d712f8845b7dd9
divya002 <divyaprakash002@gmail.com>
2017-07-22 16:00:43 +0530

    added new callback updated

--- callback_practice.js
+++ callback_practice.js
@@ -1,1 +1,27 @@
 var maxTime = 1000;
+
+var eventDoubler = function(v,callback){
+    var waitTime=Math.floor(Math.random()*(maxTime+1));
+    if(v%2){
+        setTimeout(function(){
+            callback(new Error("odd output"));
+        },waitTime);
+    }else{
+         setTimeout(function(){
+            callback(null,v*2,waitTime);
+        },waitTime);
+    }
+};
+
+var handleRsults=function(err,results,time){
+    if(err){
+        console.log("ERROR: "+err.message);
+    }else{
+        
+        console.log("The results are: "+results+"("+time+"ms)");
+    }
+};
+
+eventDoubler(2,handleRsults);
+console.log(".......................................................................");
+eventDoubler(3,handleRsults);
